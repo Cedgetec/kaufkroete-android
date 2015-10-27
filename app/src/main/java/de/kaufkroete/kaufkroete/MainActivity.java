93380b0cc1790d8555a57662829291adb08f64cb
Gebatzens <hauke@gebatzens.de>
2015-10-27 22:53:41 +0100

    New app icon

--- app/src/main/java/de/kaufkroete/kaufkroete/MainActivity.java
+++ app/src/main/java/de/kaufkroete/kaufkroete/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle("  " + getTitle());
         mToolbar.setTitleTextColor(Color.WHITE);
-        mToolbar.setLogo(R.mipmap.ic_kaufkroete);
+        mToolbar.setLogo(R.mipmap.ic_launcher);
         mToolbar.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
         //TODO Menu
         //mToolbar.inflateMenu(R.menu.menu);
